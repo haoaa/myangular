b3f7c3a7f80b0ebf0bce2f324e6ad3c7f6760d31
haoqi <290058005@qq.com>
2017-11-06 20:05:56 +0800

    Value-Based Dirty-Checking

--- karma.conf.js
+++ karma.conf.js
@@ -11,7 +11,8 @@ module.exports = function (config) {
         },
         browsers: ['PhantomJS'],
         browserify: { 
-            debug: true
+            debug: true,
+            bundleDelay: 2000 // Fixes "reload" error messages, YMMV!
           } 
     })
 }

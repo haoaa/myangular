164bd37900c88053470342197cb21810e2d84870
haoqi <290058005@qq.com>
2017-11-22 15:33:55 +0800

    Filtering With Other Primitives

---
+++ src/filter_filter.js
@@ -0,0 +1,47 @@
+"use strict";
+
+var _ = require('lodash');
+
+function filterFilter() {
+    return function (array, filterExpr) {
+        var predicateFn;
+        if (_.isFunction(filterExpr)) {
+            predicateFn = filterExpr;
+        } else if (_.isString(filterExpr) ||
+            _.isNumber(filterExpr) ||
+            _.isBoolean(filterExpr) ||
+            _.isNull(filterExpr)) {
+            predicateFn = createPredicateFn(filterExpr);
+        } else {
+            return array;
+        }
+        return _.filter(array, predicateFn);
+    };
+}
+
+function createPredicateFn(expression) {
+    function comparator(actual, expected) {
+        if (_.isUndefined(actual)) {
+            return false;
+        }
+        if (_.isNull(actual) || _.isNull(expected)) {
+            return actual === expected;
+        }
+        actual = ('' + actual).toLowerCase();
+        expected = ('' + expected).toLowerCase();
+        return actual.indexOf(expected) !== -1;
+    }
+    function deepCompare(actual, expected, comparator) {
+        if (_.isObject(actual)) {
+            return _.some(actual, function (value) {
+                return deepCompare(value, expected, comparator);
+            });
+        } else {
+            return comparator(actual, expected);
+        }
+    }
+    return function predicateFn(item) {
+        return deepCompare(item, expression, comparator);
+    };
+}
+module.exports = filterFilter;

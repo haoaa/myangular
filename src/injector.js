084538401710f25bb30474da4b2908024117b955
haoqi <290058005@qq.com>
2017-11-29 10:34:40 +0800

    Injecting $provide

--- src/injector.js
+++ src/injector.js
@@ -28,7 +28,7 @@ function createInjector(moduleToLoad, strictDi) {
 
     strictDi = (strictDi === true);
 
-    var $provide = {
+    providerCache.$provide = {
         constant : function(key, value) {
             if (key === 'hasOwnProperty') {
                 throw 'hasOwnProperty is not a valid constant name!';
@@ -132,7 +132,7 @@ function createInjector(moduleToLoad, strictDi) {
             _.forEach(module._invokeQueue, function(invokeArgs) {
                 var method = invokeArgs[0];
                 var args = invokeArgs[1];
-                $provide[method].apply($provide, args);
+                providerCache.$provide[method].apply(providerCache.$provide, args);
             });
         }
     });

e1b76b6a6e0669282dfff6fda4dd8ff35c8471db
haoqi <290058005@qq.com>
2017-11-29 16:00:39 +0800

    Function Modules. The final test case require hash-map implemented.

--- src/injector.js
+++ src/injector.js
@@ -132,17 +132,21 @@ function createInjector(moduleToLoad, strictDi) {
         });
     }
     var runBlocks = [];
-    _.forEach(moduleToLoad, function loadModule(moduleName) {
-        if (!loadedModules.hasOwnProperty(moduleName)) {
-            loadedModules[moduleName] =  true;
-            var module = window.angular.module(moduleName);
-            _.forEach(module.requires, loadModule);
-            runInvokeQueue(module._invokeQueue);
-            runInvokeQueue(module._configBlocks);
-            runBlocks = runBlocks.concat(module._runBlocks);
+    _.forEach(moduleToLoad, function loadModule(module) {
+        if (_.isString(module)) {
+            if (!loadedModules.hasOwnProperty(module)) {
+                loadedModules[module] =  true;
+                module = window.angular.module(module);
+                _.forEach(module.requires, loadModule);
+                runInvokeQueue(module._invokeQueue);
+                runInvokeQueue(module._configBlocks);
+                runBlocks = runBlocks.concat(module._runBlocks);
+            }
+        }else if(_.isFunction(module) || _.isArray(module)) {
+            runBlocks.push(providerInjector.invoke(module));
         }
     });
-    _.forEach(runBlocks, function(runBlock) {
+    _.forEach(_.compact(runBlocks), function(runBlock) {
         instanceInjector.invoke(runBlock);
     });
 

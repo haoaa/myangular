b887da990ac0c447da54abe175a194d850aab48e
haoqi <290058005@qq.com>
2017-12-01 11:58:11 +0800

    Rejecting Deferreds And Catching Rejections

--- src/q.js
+++ src/q.js
@@ -9,13 +9,17 @@ function $QProvider() {
         function Promise() {
             this.$$state = {};
         }
-        Promise.prototype.then = function(onFulfilled) {
+        Promise.prototype.then = function(onFulfilled, onRejected) {
             this.$$state.pending = this.$$state.pending || [];
-            this.$$state.pending.push(onFulfilled);
+            this.$$state.pending.push([null, onFulfilled, onRejected]);
             if (this.$$state.status > 0) {
                 scheduleProcessQueue(this.$$state);
             }
         };
+        Promise.prototype.catch = function(onRejected) {
+            return this.then(null, onRejected);
+        };
+
 
         function Deferred() {
             this.promise = new Promise();
@@ -28,6 +32,14 @@ function $QProvider() {
             this.promise.$$state.value = value;
             scheduleProcessQueue(this.promise.$$state);
         };
+        Deferred.prototype.reject = function(value) {
+            if (this.promise.$$state.status) {
+                return;
+            }
+            this.promise.$$state.status = 2;
+            this.promise.$$state.value = value;
+            scheduleProcessQueue(this.promise.$$state);
+        };
 
         function scheduleProcessQueue(state) {
             $rootScope.$evalAsync(function() {
@@ -37,8 +49,11 @@ function $QProvider() {
         function processQueue(state) {
             var pending = state.pending;
             state.pending = undefined;
-            _.each(pending, function(onFulfilled) {
-                onFulfilled(state.value);
+            _.each(pending, function(handlers) {
+                var fn = handlers[state.status];
+                if (_.isFunction(fn)) {
+                    fn(state.value);
+                }
             });
         }
 

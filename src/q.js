3aca875f59d7a9ab2a94faf2ba9da7c54e062316
haoqi <290058005@qq.com>
2017-12-02 11:03:04 +0800

    $q.all

--- src/q.js
+++ src/q.js
@@ -136,11 +136,33 @@ function $QProvider() {
             return d.promise.then(callback, errback, progressback);
         }
 
+        function all(promises) {
+            var results = _.isArray(promises) ? [] : {};
+            var counter = 0;
+            var d = new Deferred();
+            _.each(promises, function(promise, index) {
+                counter++;
+                when(promise).then(function(value) {
+                    results[index] = value;
+                    counter--;
+                    if (!counter) {
+                        return d.resolve(results);
+                    }
+                }, function(rejection) {
+                    d.reject(rejection);
+                });
+            });
+            if (!counter) {
+                d.resolve(results);
+            }
+            return d.promise;
+        }
         return {
             defer : defer,
             reject: reject,
             when : when,
-            resolve : when
+            resolve : when,
+            all : all
         };
     }];
 }

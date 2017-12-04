407f3f76051d5ae96e1238308ded2f9a1f41c52f
haoqi <290058005@qq.com>
2017-12-04 10:21:14 +0800

    Request Headers

--- src/http.js
+++ src/http.js
@@ -2,17 +2,80 @@
 
 var _ = require('lodash');
 
+function isSuccess(status) {
+    return status >= 200 && status < 300;
+}
+
 function $HttpProvider() {
+
+    var defaults = this.defaults = {
+        headers : {
+            common : {
+                'Accept': 'application/json, text/plain, */*'
+            },
+            post : {
+                'Content-Type': 'application/json;charset=utf-8'
+            },
+            put: {
+                'Content-Type': 'application/json;charset=utf-8'
+            },
+            patch: {
+                'Content-Type': 'application/json;charset=utf-8'
+            }
+        }
+    };
+
     this.$get = ['$httpBackend', '$q', '$rootScope',  function($httpBackend, $q, $rootScope) {
-        return function $http(requestConfig) {
+
+        function $http(requestConfig) {
             var deferred = $q.defer();
 
             var config = _.extend({
                 method : 'GET'
             }, requestConfig);
+            config.headers = mergeHeaders(requestConfig);
 
-            function isSuccess(status) {
-                return status >= 200 && status < 300;
+            if (_.isUndefined(config.data)) {
+                _.forEach(config.headers, function(v, k) {
+                    if (k.toLowerCase() === 'content-type') {
+                        delete config.headers[k];
+                    }
+                });
+            }
+
+            function mergeHeaders(config) {
+                var reqHeaders = _.extend(
+                    {},
+                    config.headers
+                );
+                var defHeaders = _.extend(
+                    {},
+                    defaults.headers.common,
+                    defaults.headers[(config.method || 'get').toLowerCase()]
+                );
+                _.each(defHeaders, function(value, key) {
+                    var headerExists = _.some(reqHeaders, function(v, k) {
+                        return k.toLowerCase() === key.toLowerCase();
+                    });
+                    if (!headerExists) {
+                        reqHeaders[key] = value;
+                    }
+                });
+                return executeHeaderFns(reqHeaders, config);
+            }
+
+            function executeHeaderFns(header, config) {
+                return _.transform(header, function(result, v, k) {
+                    if (_.isFunction(v)) {
+                        v = v(config);
+
+                        if (_.isNull(v) || _.isUndefined(v)) {
+                            delete result[k];
+                        }else {
+                            result[k] = v;
+                        }
+                    }
+                }, header);
             }
 
             function done(status, response, statusText) {
@@ -28,9 +91,18 @@ function $HttpProvider() {
                 }
             }
 
-            $httpBackend(config.method, config.url, config.data, done);
+            $httpBackend(
+                config.method,
+                config.url,
+                config.data,
+                done,
+                config.headers
+            );
             return deferred.promise;
-        };
+        }
+
+        $http.defaults = defaults;
+        return $http;
     }];
 }
 module.exports = $HttpProvider;

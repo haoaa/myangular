f5727377399cf90d7c6cb00cb3ad1a639cf0e814
haoqi <290058005@qq.com>
2017-12-08 15:32:46 +0800

    Directive Link Functions

--- test/compile_spec.js
+++ test/compile_spec.js
@@ -864,6 +864,27 @@ describe('$compile', function() {
             });
 
         });
-
+        it('calls directive link function with scope', function() {
+            var givenScope, givenElement, givenAttrs;
+            var injector = makeInjectorWithDirectives('myDirective', function() {
+                return {
+                    compile: function() {
+                        return function link(scope, element, attrs) {
+                            givenScope = scope;
+                            givenElement = element;
+                            givenAttrs = attrs;
+                        };
+                    }
+                };
+            });
+            injector.invoke(function($compile, $rootScope) {
+                var el = $('<div my-directive></div>');
+                $compile(el)($rootScope);
+                expect(givenScope).toBe($rootScope);
+                expect(givenElement[0]).toBe(el[0]);
+                expect(givenAttrs).toBeDefined();
+                expect(givenAttrs.myDirective).toBeDefined();
+            });
+        });
     });
 });

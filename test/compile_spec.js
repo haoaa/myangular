3fc7123714960ac95c171b6683fcad0a1480b615
haoqi <290058005@qq.com>
2017-12-12 09:47:32 +0800

    Self-Requiring Directives

--- test/compile_spec.js
+++ test/compile_spec.js
@@ -1948,5 +1948,26 @@ describe('$compile', function() {
                 expect(gotControllers.myDirective instanceof MyController).toBe(true);
             });
         });
+        it('requires itself if there is no explicit require', function() {
+            function MyController() { }
+            var gotMyController;
+            var injector = createInjector(['ng', function($compileProvider) {
+                $compileProvider.directive('myDirective', function() {
+                    return {
+                        scope: {},
+                        controller: MyController,
+                        link: function(scope, element, attrs, myController) {
+                            gotMyController = myController;
+                        }
+                    };
+                });
+            }]);
+            injector.invoke(function($compile, $rootScope) {
+                var el = $('<div my-directive></div>');
+                $compile(el)($rootScope);
+                expect(gotMyController).toBeDefined();
+                expect(gotMyController instanceof MyController).toBe(true);
+            });
+        })
     });
 });

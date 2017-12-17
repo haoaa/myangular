fbc35befa65d7d52ef29ec8a939e57edb77f415a
haoqi <290058005@qq.com>
2017-12-17 15:08:13 +0800

    Value Stringification

--- src/interpolate.js
+++ src/interpolate.js
@@ -4,45 +4,56 @@ var _ = require('lodash');
 
 function $InterpolateProvider() {
 
-    this.$get = ['$parse', function($parse) {
+    function stringify(value) {
+        if (_.isNull(value) || _.isUndefined(value)) {
+            return '';
+        } else if(_.isObject(value)) {
+            return JSON.stringify(value);
+        } else {
+            return '' + value;
+        }
+    }
 
-        function $interpolate(text) {
-            var index = 0;
-            var parts = [];
-            var startIndex, endIndex, exp, expFn;
+    this.$get = [
+        '$parse', function($parse) {
 
-            while (index < text.length) {
-                startIndex = text.indexOf('{{');
-                if (startIndex !== -1) {
-                    endIndex = text.indexOf('}}', startIndex + 2);
-                }
-                if (startIndex !== -1 && endIndex !== -1) {
-                    if (startIndex !== index) {
-                        parts.push(text.substring(index, startIndex));
+            function $interpolate(text) {
+                var index = 0;
+                var parts = [];
+                var startIndex, endIndex, exp, expFn;
+
+                while (index < text.length) {
+                    startIndex = text.indexOf('{{', index);
+                    if (startIndex !== -1) {
+                        endIndex = text.indexOf('}}', startIndex + 2);
                     }
-                    exp = text.substring(startIndex + 2, endIndex);
-                    expFn = $parse(exp);
-                    parts.push(expFn);
-                    index = endIndex + 2;
-                } else {
-                    parts.push(text.substring(index));
-                    break;
+                    if (startIndex !== -1 && endIndex !== -1) {
+                        if (startIndex !== index) {
+                            parts.push(text.substring(index, startIndex));
+                        }
+                        exp = text.substring(startIndex + 2, endIndex);
+                        expFn = $parse(exp);
+                        parts.push(expFn);
+                        index = endIndex + 2;
+                    } else {
+                        parts.push(text.substring(index));
+                        break;
+                    }
                 }
+
+                return function interpolationFn(context) {
+                    return _.reduce(parts, function(result, part) {
+                        if (_.isFunction(part)) {
+                            return result + stringify(part(context));
+                        } else {
+                            return result + part;
+                        }
+                    }, '');
+                };
             }
 
-
-            return function interpolationFn(context) {
-                return _.reduce(parts, function(result, part) {
-                    if (_.isFunction(part)) {
-                        return result + part(context);
-                    } else {
-                        return result + part;
-                    }
-                }, '');
-            };
-        }
-        return $interpolate;
-    }];
+            return $interpolate;
+        }];
 }
 
 module.exports = $InterpolateProvider;
